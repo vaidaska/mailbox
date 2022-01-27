ff4496bf5921f892e10725b54123de635683bdd7
vaidaska <vaidas.kausinskas@gmail.com>
2022-01-27 09:47:35 +0200

    Solution #1

--- src/main/java/lt/vaidask/mailbox/model/Email.java
+++ src/main/java/lt/vaidask/mailbox/model/Email.java
@@ -1,11 +1,13 @@
 package lt.vaidask.mailbox.model;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.ToString;
 
 import java.util.List;
 
 @Builder
+@Getter
 @EqualsAndHashCode
 @ToString(exclude = "content")
 public class Email {
--- src/main/java/lt/vaidask/mailbox/service/MailboxService.java
+++ src/main/java/lt/vaidask/mailbox/service/MailboxService.java
@@ -26,6 +26,43 @@ public class MailboxService {
 
         List<Mailbox> mailboxes= new ArrayList<>();
 
+        //Mailbox owner
+        User mailboxUser = users.get(0);
+
+        //Get sent email list of the mailbox owner
+        List<Email> sentEmails = getUserSentEmails(mailboxUser, emails);
+
+        //Get received email list of the mailbox owner
+        List<Email> receivedEmails = getUserReceivedEmails(mailboxUser, emails);
+
+        //Create mailbox of mailbox owner with sent/receives emails
+        mailboxes.add(new Mailbox( users.get(0), sentEmails, receivedEmails));
+
         return mailboxes;
+    }
+
+    public List<Email> getUserSentEmails(User user, List<Email> emails) {
+        List<Email> sentEmails = new ArrayList<>();
+
+        emails.forEach(email -> {
+            if (email.getAuthor().equals(user)) {
+                sentEmails.add(email);
+            }
+        });
+        return sentEmails;
+    }
+
+    public List<Email> getUserReceivedEmails(User user, List<Email> emails) {
+        List<Email> receivedEmails = new ArrayList<>();
+
+        emails.forEach(email -> {
+            email.getRecipients().forEach(recipient -> {
+                if(recipient.equals(user)) {
+                    receivedEmails.add(email);
+                }
+            });
+        });
+
+        return receivedEmails;
     }
 }
